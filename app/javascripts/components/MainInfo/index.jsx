6d5e3ccaa7b3f7595acf52ee844cea5f25b98ba4
sanyao <sanyaobox@gmail.com>
2017-07-02 23:43:58 +0800

    add speakers of maininfo

--- app/javascripts/components/MainInfo/index.jsx
+++ app/javascripts/components/MainInfo/index.jsx
@@ -4,6 +4,7 @@ import header from "jsons/header.json";
 import styles from "./styles.css";
 import side_events from "jsons/side_events.json";
 import about from "./about.css";
+import keynote_speakers from "jsons/keynote_speakers.json";
 
 var multiParagraph = (text, className) => {
   if (!text) {
@@ -20,6 +21,7 @@ var multiParagraph = (text, className) => {
 
 
 export default class MainInfo extends Component {
+
   SideEvent = (SideEvent, i) => {
     return (
       	<div className={styles.sideevents}>
@@ -54,100 +56,129 @@ export default class MainInfo extends Component {
 			</div>
 		</div>
     );
+  };
+
+  Speaker = (Speaker, i) => {
+    return (
+      	<a className={styles.speakers}>
+			<span className={styles.inner}>
+					<span className={styles.avatar}>
+						<span>
+						<img src={require(`${Speaker.avatar}`)} />
+						</span>
+					</span>
+					<span className={styles.about}>
+						<h3>{Speaker.name}</h3>
+						<p>{Speaker.title}, {Speaker.organization}, {Speaker.nation}</p>
+					</span>
+			</span>
+			<span className={styles.venue}>
+				{ Speaker.event.map( (event) => {
+	        		return (
+			   			<span>{ event }</span>
+			   		)
+		        })}
+			</span>
+		</a>
+    );
   }
 
   render() {
     return (
       <div className={styles.root}>
       	<div className={styles.banner}>
-		  		<img className={styles.hero} src={require('./images/hero.jpg')} />
-		  		<div className={styles.herotext}>
-		  			<h1>#CivicTechFest</h1>
-					<h2>Sep. 11th - 16th<br/>Taipei, Taiwan</h2>
+	  		<img className={styles.hero} src={require('./images/hero.jpg')} />
+	  		<div className={styles.herotext}>
+	  			<h1>#CivicTechFest</h1>
+				<h2>Sep. 11th - 16th<br/>Taipei, Taiwan</h2>
+			</div>
+		</div>
+		<article className={styles.relative} data-wide="true">
+		  <div className={styles.brief}>
+		    <p>Asia’s first ever civic technology festival with  a week-long collection of events.</p>
+		  </div>
+		  <div className={styles.dateline}>
+		  	<ul>
+		  		<li className={styles.month}><a><span>Sep.</span><p>Events</p></a></li>
+		  		<li><a><span>9</span><span>10</span><p></p></a></li>
+		  		<li><a><span>11</span><p>Civic Tech Grant DEMO Party</p></a></li>
+		  		<li className={styles.main}><a><span>11</span><span>12</span><span>13</span><p>TICTeC</p><h6>alongside with WCIT</h6></a></li>
+		  	</ul>
+		  	<ul>
+		  		<li><a><span>14</span><p>Asia Open Data Hackthon<br />Code for ALL</p></a></li>
+		  		<li><a><span>15</span><p></p></a></li>
+		  		<li><a><span>16</span><p>g0v Hackthon</p></a></li>
+		  	</ul>
+		  </div>
+		</article>
+		<article className={styles.container}>
+			<h2 className={styles.orange}>Main Event</h2>
+			<div className={styles.mainevent}>
+				<div className={styles.eventimg}>
+					<img data-type="logo" src={require(`file!images/sponsors/tictec.jpg`)} />
+				</div>
+				<div className={styles.eventcontent}>
+					<h3>TICTeC @ Taipei</h3>
+					<p>The main highlight for the Civic Tech Fest is “TICTeC@Taipei: The Impacts of Civic Technology Conference”, co-organized by UK group “mySociety”. TICTeC is an annual international conference that focuses on the impact of civic tech since 2015 and will be hold in Asia in the first time. TICTeC@Taipei is a great opportunity to connect global network in the Asia base. </p>
+					<a className={styles.eventbtn} href="https://ocftw.kktix.cc/events/civictechfest2017" target="_blank"><span>Get Ticket !</span></a>
+				</div>
+				<div className={styles.eventdate}>						
+					<div>
+						<span>Sep.</span>
+						<span>11</span><span>12</span><span>13</span>
+					</div>
+				</div>
+				<div className={styles.venue}>
+					<span><i data-color="1">place</i>TICC</span>
+				</div>
+				<div className={styles.tab}>
+					<span className={styles.tabblue}>alongside with WCIT</span>
+					<br />
+					<span>Ticket</span>
 				</div>
 			</div>
-			<article className={styles.relative} data-wide="true">
-			  <div className={styles.brief}>
-			    <p>Asia’s first ever civic technology festival with  a week-long collection of events.</p>
-			  </div>
-			  <div className={styles.dateline}>
-			  	<ul>
-			  		<li className={styles.month}><a><span>Sep.</span><p>Events</p></a></li>
-			  		<li><a><span>9</span><span>10</span><p></p></a></li>
-			  		<li><a><span>11</span><p>Civic Tech Grant DEMO Party</p></a></li>
-			  		<li className={styles.main}><a><span>11</span><span>12</span><span>13</span><p>TICTeC</p><h6>alongside with WCIT</h6></a></li>
-			  	</ul>
-			  	<ul>
-			  		<li><a><span>14</span><p>Asia Open Data Hackthon<br />Code for ALL</p></a></li>
-			  		<li><a><span>15</span><p></p></a></li>
-			  		<li><a><span>16</span><p>g0v Hackthon</p></a></li>
-			  	</ul>
-			  </div>
-			</article>
-			<article className={styles.container}>
-				<h2 className={styles.orange}>Main Event</h2>
-				<div className={styles.mainevent}>
-					<div className={styles.eventimg}>
-						<img data-type="logo" src={require(`file!images/sponsors/tictec.jpg`)} />
-					</div>
-					<div className={styles.eventcontent}>
-						<h3>TICTeC @ Taipei</h3>
-						<p>The main highlight for the Civic Tech Fest is “TICTeC@Taipei: The Impacts of Civic Technology Conference”, co-organized by UK group “mySociety”. TICTeC is an annual international conference that focuses on the impact of civic tech since 2015 and will be hold in Asia in the first time. TICTeC@Taipei is a great opportunity to connect global network in the Asia base. </p>
-						<a className={styles.eventbtn} href="https://ocftw.kktix.cc/events/civictechfest2017" target="_blank"><span>Get Ticket !</span></a>
-					</div>
-					<div className={styles.eventdate}>						
-						<div>
-							<span>Sep.</span>
-							<span>11</span><span>12</span><span>13</span>
-						</div>
-					</div>
-					<div className={styles.venue}>
-						<span><i data-color="1">place</i>TICC</span>
-					</div>
-					<div className={styles.tab}>
-						<span className={styles.tabblue}>alongside with WCIT</span>
-						<br />
-						<span>Ticket</span>
-					</div>
+			<div className={about.about}>
+				<div className={about.title}>
+					<h3>About WCIT 2017 TAIWAN</h3>
+					<a href="http://www.wcit2017.org/" target="_blank"><i className={about.icon}>home</i>www.wcit2017.org</a>
 				</div>
-				<div className={about.about}>
-					<div className={about.title}>
-						<h3>About WCIT 2017 TAIWAN</h3>
-						<a href="http://www.wcit2017.org/" target="_blank"><i className={about.icon}>home</i>www.wcit2017.org</a>
-					</div>
-					<div className={about.content}>
-						<p>The 21st World Congress on Information Technology (WCIT 2017) is the parallel event of #CivicTechFest. It is held in the same time and at same location as TICTeC@Taipei and will be a really unique opportunity to showcase open government and civic tech initiatives to the wider IT industry.</p>
-					</div>
+				<div className={about.content}>
+					<p>The 21st World Congress on Information Technology (WCIT 2017) is the parallel event of #CivicTechFest. It is held in the same time and at same location as TICTeC@Taipei and will be a really unique opportunity to showcase open government and civic tech initiatives to the wider IT industry.</p>
+				</div>
+			</div>
+		</article>
+
+		{ 
+	      side_events[getLocale()].map( cat => {
+	        return (
+			<article id={cat.category} className={styles.container} key={cat.category}>
+
+				<h2 className={styles.blue}>{cat.title}</h2>
+				<div className={styles.blue_dateline}>
+					<span>9</span>
+					<span>10</span>
+					<span>11</span>
+					<span className={styles.grey}>12</span>
+					<span className={styles.grey}>13</span>
+					<span>14</span>
+					<span>15</span>
+					<span>16</span>
+				</div>					
+		    	<div>
+	            { cat.side_events.map(this.SideEvent) }
 				</div>
 			</article>
+			)
+          }) 
+        }
 
-			{ 
-		      side_events[getLocale()].map( cat => {
-		        return (
-				<article id={cat.category} className={styles.container} key={cat.category}>
 
-					<h2 className={styles.blue}>{cat.title}</h2>
-					<div className={styles.blue_dateline}>
-						<span>9</span>
-						<span>10</span>
-						<span>11</span>
-						<span className={styles.grey}>12</span>
-						<span className={styles.grey}>13</span>
-						<span>14</span>
-						<span>15</span>
-						<span>16</span>
-					</div>					
-			    	<div>
-		            { cat.side_events.map(this.SideEvent) }
-					</div>
-				</article>
-				)
-	          }) 
-	        }
+        { 
+	      keynote_speakers[getLocale()].map( cat => {
+	        return (
+			<article id={cat.category} className={styles.container} key={cat.category} data-wide="true">
 
-			<article className={styles.container} data-wide="true">
-
-				<h2 className={styles.orange}>Keynote speakers</h2>
+				<h2 className={styles.orange}>{cat.title}</h2>
 				<div className={styles.orange_dateline}>
 					<span className={styles.grey}>9</span>
 					<span className={styles.grey}>10</span>
@@ -159,95 +190,14 @@ export default class MainInfo extends Component {
 					<span className={styles.grey}>16</span>
 				</div>
 
-				<a className={styles.speakers}>
-					<span className={styles.inner}>
-							<span className={styles.avatar}>
-								<span>
-								<img src="" />
-								</span>
-							</span>
-							<span className={styles.about}>
-								<h3>Mathilde Bras</h3>
-								<p>open government officer, Etalab, French</p>
-							</span>
-					</span>
-					<span className={styles.venue}>
-						<span>WCIT</span>
-						<span>TICTeC</span>
-					</span>
-				</a>
-
-				<a className={styles.speakers}>
-					<span className={styles.inner}>
-							<span className={styles.avatar}>
-								<span>
-								<img src="" />
-								</span>
-							</span>
-							<span className={styles.about}>
-								<h3>Mathilde Bras</h3>
-								<p>open government officer, Etalab, French</p>
-							</span>
-					</span>
-					<span className={styles.venue}>
-						<span>WCIT</span>
-						<span>TICTeC</span>
-					</span>
-				</a>
-
-				<a className={styles.speakers}>
-					<span className={styles.inner}>
-							<span className={styles.avatar}>
-								<span>
-								<img src="" />
-								</span>
-							</span>
-							<span className={styles.about}>
-								<h3>Mathilde Bras</h3>
-								<p>open government officer, Etalab, French</p>
-							</span>
-					</span>
-					<span className={styles.venue}>
-						<span>WCIT</span>
-						<span>TICTeC</span>
-					</span>
-				</a>
-
-				<a className={styles.speakers}>
-					<span className={styles.inner}>
-							<span className={styles.avatar}>
-								<span>
-								<img src="" />
-								</span>
-							</span>
-							<span className={styles.about}>
-								<h3>Mathilde Bras</h3>
-								<p>open government officer, Etalab, French</p>
-							</span>
-					</span>
-					<span className={styles.venue}>
-						<span>TICTeC</span>
-					</span>
-				</a>
-
-				<a className={styles.speakers}>
-					<span className={styles.inner}>
-							<span className={styles.avatar}>
-								<span>
-								<img src="" />
-								</span>
-							</span>
-							<span className={styles.about}>
-								<h3>Mathilde Bras</h3>
-								<p>open government officer, Etalab, French</p>
-							</span>
-					</span>
-					<span className={styles.venue}>
-						<span>WCIT</span>
-					</span>
-				</a>
-
+				<div>
+				{ cat.keynote_speakers.map(this.Speaker) }
+				</div>
 			</article>
+			)
+          }) 
+        }
+			
 
      </div>
     );
--- app/javascripts/pages/Home/index.jsx
+++ app/javascripts/pages/Home/index.jsx
@@ -20,21 +20,3 @@ class Home extends Component {
 
 export default Home;
 
-
-/*
-import __html from 'raw-loader!./template.html';
-// workaround https://github.com/webpack-contrib/raw-loader/issues/13
-var trimmed_html = __html.replace(/\\n/g, '\n').replace(/\\/g, "");
-trimmed_html = trimmed_html.substring(18, trimmed_html.length-1);
-var template = { __html: trimmed_html };
-
-var Home = React.createClass({
-  render: function() {
-    return(
-      <div dangerouslySetInnerHTML={template} />
-    );
-  }
-});
-
-export default Home;
-*/
