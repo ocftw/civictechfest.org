ca1a8cf4a7c1c780ff52e504f4efc626a3a49eb7
Pellaeon Lin <nfsmwlin@gmail.com>
2017-07-03 21:19:21 +0800

    Fix title

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -10,9 +10,9 @@ import {basic} from "./jld.js";
 var plugins = [
   new ExtractTextPlugin("[name].[hash].css", {disable: process.env.NODE_ENV !== 'production'}), // Disable extract-text-plugin during development for hot-reloading CSS.
   new HtmlWebpackPlugin({
-    title: "啥米零時政府 g0v 2016 summit",
+    title: "Civic Tech Fest",
     jld: basic,
-    description: "2014 之後又跳過了一整年，g0v summit 2016 再次邀請全球公民技術社群來分享公務員，技術人員，和非政府組織工作人員之間的協作經驗。我們很高興邀請到 Filipe Heusser 擔任今年的專題演講者，他也是 the Chilean NGO Ciudadano Inteligente 的創辦人和前股東，以及 Berkman Center 的成員。",
+    description: "Asia’s first ever civic technology festival and conference.",
     filename: "index.html",
     template: "app/templates/index.html"
   }),
