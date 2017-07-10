db5557589b69a320a85430522419cf69efd9dea1
Pellaeon Lin <nfsmwlin@gmail.com>
2017-07-10 17:44:22 +0800

    Add Google analytics

--- app/javascripts/application.js
+++ app/javascripts/application.js
@@ -7,7 +7,7 @@ import { Router,  browserHistory } from 'react-router';
 import locale, { getLocale, setLocale } from "./locale";
 import getRoutes from './routes';
 import ga from 'react-ga';
-ga.initialize('UA-41326468-1');
+ga.initialize('UA-102190863-2');
 
 browserHistory.listen(location => ga.pageview(location.pathname));
 const component = (
