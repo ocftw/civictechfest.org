7592eafe5004ffb0d21868ddfda2fa88189868ad
Pellaeon Lin <nfsmwlin@gmail.com>
2017-08-22 17:40:38 +0800

    speakerList: WIP: refactor mapToDescription to getSessionIdsByPresenter

--- app/javascripts/components/SpeakerList/index.jsx
+++ app/javascripts/components/SpeakerList/index.jsx
@@ -26,6 +26,9 @@ class SpeakerList extends Component {
       setTimeout(() => document.getElementById(hash.replace('#', 'slot-')).scrollIntoView(false), 300);
       const dataArray = hash.replace('#', '').split('-');
       const value = schedulesByTrack[getLocale()][dataArray[0]][dataArray[2]];
+      // TODO get presenter name,
+      // search for sessions,
+      // put in state currentSessions
       this.setState({
         showSession: true,
         currentSession: () => schedulesByTrack[getLocale()][dataArray[0]][dataArray[2]].event,
@@ -73,7 +76,7 @@ class SpeakerList extends Component {
   speaker = (speaker) => {
     const avatar = avatarURL(speaker);
     const [locale] = getLocale().split('-');
-    let data = this.mapToDescription(speaker, locale);
+    let data = this.getSessionIdsBySpeaker(speaker, locale);
 
     return (
       <a className={styles.speakers} key={speaker.name}
@@ -107,131 +110,43 @@ class SpeakerList extends Component {
             time,
             id(day-all-index) or id(none-index)  if nothing found in schedules_by_track.json
   */
-  mapToDescription(speaker, locale) {
-    let data = [];
-    let event = null;
-    let id = "";
+  getSessionIdsBySpeaker(speaker, locale) {
+    let sessiondata = [];
+    let ids = [];
 
-    /*
-    for (var day_key in schedulesByTrack[getLocale()]) {
-      if ( !day_key.startsWith('day') ) continue;//skip header, etc TODO schedules_by_track.json bad structure
-      console.log("day: "+day_key);
-      for (var event_key in schedulesByTrack[getLocale()][day_key]) {
-        console.log("event: "+event_key);
-        data = schedulesByTrack[getLocale()][day_key][event_key];
-        if ( !Array.isArray(data.event) ) continue;
-        var thisevent = data.event;
-        console.log(thisevent);
-        if ( thisevent.hasOwnProperty('speaker') ) {
-          console.log('cheching speaker');
-          if ( Array.isArray(thisevent.speaker) ) {
-            if ( thisevent.speaker.includes(getString(speaker, 'name', locale)) ) {
-              event = thisevent;
-              id = day_key+"-"+event_key;
-              //data ?
-            }
-          } else if ( thisevent.speaker == getString(speaker, 'name', locale) ) {
-            event = thisevent;
-            id = day_key+"-"+event_key;
-          }
-          console.log('checked speaker');
-        } else if ( thisevent.hasOwnProperty('moderator') ) {
-          if ( Array.isArray(thisevent.moderator) ) {
-            if ( thisevent.moderator.includes(getString(speaker, 'name', locale)) ) {
-              event = thisevent;
-              id = day_key+"-"+event_key;
-              //data ?
-            }
-          } else if ( thisevent.moderator == getString(speaker, 'name', locale) ) {
-            event = thisevent;
-            id = day_key+"-"+event_key;
-          }
-          console.log('checked moderator');
-        } else {
-          console.log("no session found");
+    for ( i=0; i<=3; i++ ) {
+      Array.prototype.push.apply(sessiondata, schedulesByTrack[getLocale()]["day"+i].filter((day, index) => {
+        if(day.event.speaker_f && day.event.speaker_f.includes(getString(speaker, 'name', locale))||
+           (day.event.moderator_f && day.event.moderator_f === getString(speaker, 'name', locale)) //||
+          ) {
+          ids.push("day"+i+"-all-" + index.toString());
+          return true;
+        }else {
+          return false;
         }
-      }
+      })
+      );
     }
-    console.log(id);
-    return {
-      event: event,
-      id: id,
-      time: data.time
-    };
-    // search day1 first
-    */
-    data = schedulesByTrack[getLocale()]["day0"].filter((day, index) => {
-      if(day.event.speaker_f && day.event.speaker_f.includes(getString(speaker, 'name', locale))||
-         (day.event.moderator_f && day.event.moderator_f === getString(speaker, 'name', locale)) //||
-        ) {
-        id = "day0-all-" + index.toString();
-        return true;
-      }else {
-        return false;
-      }
-    });
     
-    if(id == "") {
-      data = schedulesByTrack[getLocale()]["day1"].filter((day, index) => {
-        if(day.event.speaker_f && day.event.speaker_f.includes(getString(speaker, 'name', locale))||
-          (day.event.moderator_f && day.event.moderator_f === getString(speaker, 'name', locale)) //||
-          ) {
-          id = "day1-all-" + index.toString();
-          return true;
-        }else {
-          return false;
-        }
-      });
-    }
 
-    // nothing found in day1, then try search day2
-    if(id == "") {
-      data = schedulesByTrack[getLocale()]["day2"].filter((day, index) => {
-        if(day.event.speaker_f && day.event.speaker_f.includes(getString(speaker, 'name', locale))||
-           ((day.event.moderator_f && day.event.moderator_f) === getString(speaker, 'name', locale))
-          ) {
-          id = "day2-all-" + index.toString();
-          return true;
-        }else {
-          return false;
-        }
-      });
-    }
-    if(id == "") {
-      data = schedulesByTrack[getLocale()]["day3"].filter((day, index) => {
-        if(day.event.speaker_f && day.event.speaker_f.includes(getString(speaker, 'name', locale))||
-           ((day.event.moderator_f && day.event.moderator_f) === getString(speaker, 'name', locale))
-          ) {
-          id = "day3-all-" + index.toString();
-          return true;
-        }else {
-          return false;
-        }
-      });
-    }
-
-    // data found in day1 or day2
-    if(id !== "") {
+    if(ids.length >= 0) {
       let day_venue_index = id.split("-");
 
       return {
-        event: () => schedulesByTrack[getLocale()][day_venue_index[0]][day_venue_index[2]].event,
-        time: schedulesByTrack[getLocale()][day_venue_index[0]][day_venue_index[2]].time,
-        id: id
+        sessions: sessiondata,
+        ids: ids
       };
     }
 
     // nothing found in schedules_by_track.json,so return speaker.json's data instead
-    if((data == null) ||
-       (event == null) ||
-       (id == "")){
+    if( ids.length == 0 ) {
 
       let speakerElement = [];
       presenters['en-US'].map((element, i) => {
           speakerElement[i] = getString(element, 'name', locale);
       });
 
-      return {
+      let session_placeholder = {
         event: () => ({
           venue: "",
           category: "",
@@ -242,8 +157,12 @@ class SpeakerList extends Component {
           abstract: "",
           avatar: speaker.avatar,
           value: speaker }),
-        time: "",
-        id: "none-" + speakerElement.indexOf(getString(speaker, 'name', locale))
+        time: ""
+      }
+
+      return {
+        sessions: [session_placeholder],
+        ids: ["none-" + speakerElement.indexOf(getString(speaker, 'name', locale))]
       };
     }
   }
