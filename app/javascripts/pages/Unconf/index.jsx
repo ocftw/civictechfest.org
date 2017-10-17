e9100e86c339f48dbd127d4866789f9e46b06015
Pellaeon Lin <nfsmwlin@gmail.com>
2017-10-17 15:43:51 +0800

    Unconf spreadsheet embed, partner, side event udpate

--- app/javascripts/pages/Unconf/index.jsx
+++ app/javascripts/pages/Unconf/index.jsx
@@ -11,6 +11,7 @@ class Unconf extends Component {
                 <div dangerouslySetInnerHTML={{__html: content[getLocale()]}}>
                 </div>
                 <img src={require("./unconf_room.png")} />
+                <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vQqUFSYca3fyDOfe7fwhr3vRrD_Q6LA5LdhmqqyspZPlekWDgPSuZegydinMT8ikxHWJ0s9h8fnBvjd/pubhtml?widget=true&amp;headers=false" width="100%" height="400px"></iframe>
               </div>
           );
     }
